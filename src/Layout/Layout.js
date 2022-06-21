74cc354e36b6bcc5720e18688b4ae1544dfad5b2
Tyler Mason <tymasn@gmail.com>
2022-06-21 12:18:08 -0600

    added some better spacing and alignment to cards.

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,13 +2,44 @@ import React from 'react'
 import {Grid} from '@mui/material'
 import Card from '../Card/Card.js'
 
+const data = {
+  cardOne: {
+    name: 'cardOne',
+    url: 'link here',
+    number: 1
+  },
+  cardTwo: {
+    name: 'cardOne',
+    url: 'link here',
+    number: 1
+  },
+  cardThree: {
+    name: 'cardOne',
+    url: 'link here',
+    number: 1
+  },
+  cardFour: {
+    name: 'cardOne',
+    url: 'link here',
+    number: 1
+  }
+}
+
 const Layout = () => {
   return (
-    <Grid container spacing={3}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+    <Grid container justifyContent="center" spacing={3} padding={3}>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
+        <Grid item>
+          <Card></Card>
+        </Grid>
     </Grid>
   )
 }
