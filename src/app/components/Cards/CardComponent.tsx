cde5319237dd0585ac2460436a1f4d1a523a2916
TylerMason <tymasn@gmail.com>
2023-12-06 10:35:08 -0800

    Removed default layout and added cards of my own

--- src/app/components/Cards/CardComponent.tsx
+++ src/app/components/Cards/CardComponent.tsx
@@ -9,12 +9,12 @@ interface CardProps {
 const CardComponent: React.FC<CardProps> = ({ title, imageUrl, href }) => {
   return (
     <Link href={href}>
-      <a className="rounded overflow-hidden shadow-lg bg-white hover:bg-gray-100 transition-colors duration-300 cursor-pointer">
+      <div className="rounded overflow-hidden shadow-lg bg-white hover:bg-gray-100 transition-colors duration-300 cursor-pointer">
         <img className="w-full" src={imageUrl} alt="Card" />
         <div className="p-4">
           <div className="font-bold text-xl mb-2">{title}</div>
         </div>
-      </a>
+      </div>
     </Link>
   );
 };
