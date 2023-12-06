c030d4d071be0ea133330ac5ca8081b7942935fc
TylerMason <tymasn@gmail.com>
2023-12-06 10:57:55 -0800

    Got the alignment done and spaced them to be responsive.

--- src/app/components/Cards/CardComponent.tsx
+++ src/app/components/Cards/CardComponent.tsx
@@ -9,8 +9,8 @@ interface CardProps {
 const CardComponent: React.FC<CardProps> = ({ title, imageUrl, href }) => {
   return (
     <Link href={href}>
-      <div className="rounded overflow-hidden shadow-lg bg-white hover:bg-gray-100 transition-colors duration-300 cursor-pointer">
-        <img className="w-full" src={imageUrl} alt="Card" />
+      <div className="aspect-w-5 aspect-h-3 overflow-hidden rounded shadow-lg bg-white hover:bg-gray-100 transition-colors duration-300 cursor-pointer">
+        <img className="object-cover" src={imageUrl} alt="Card" />
         <div className="p-4">
           <div className="font-bold text-xl mb-2">{title}</div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
-import Image from 'next/image'
-import CardComponent from './components/Cards/CardComponent'
+import Image from 'next/image';
+import CardComponent from './components/Cards/CardComponent';
+
+// Placeholder data for the cards
+const cardData = [
+  { title: "Card 1", imageUrl: "/path-to-image1.jpg", href: "/page1" },
+  { title: "Card 2", imageUrl: "/path-to-image2.jpg", href: "/page2" },
+  { title: "Card 3", imageUrl: "/path-to-image3.jpg", href: "/page3" },
+  { title: "Card 4", imageUrl: "/path-to-image4.jpg", href: "/page4" },
+  { title: "Card 5", imageUrl: "/path-to-image5.jpg", href: "/page5" },
+  { title: "Card 6", imageUrl: "/path-to-image6.jpg", href: "/page6" },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className=" flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static h-auto lg:w-auto lg:bg-none">
         
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <div className="container mx-auto p-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-            {/* Replace with your actual data */}
-            <CardComponent title="Card 1" imageUrl="/path-to-image1.jpg" href="/page1" />
-            <CardComponent title="Card 2" imageUrl="/path-to-image2.jpg" href="/page2" />
-            {/* More cards as needed */}
-          </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {/* Map over the card data to render each card */}
+          {cardData.map((card, index) => (
+            <CardComponent
+              key={index}
+              title={card.title}
+              imageUrl={card.imageUrl}
+              href={card.href}
+            />
+          ))}
         </div>
-        
       </div>
-
+      
     </main>
-  )
+  );
 }
