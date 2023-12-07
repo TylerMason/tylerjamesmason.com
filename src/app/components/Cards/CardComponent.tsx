42b89b24fba2611d8cd631dcf7070ca7c58436b7
TylerMason <tymasn@gmail.com>
2023-12-06 16:59:28 -0800

    Cool image covers for my cards

--- src/app/components/Cards/CardComponent.tsx
+++ src/app/components/Cards/CardComponent.tsx
@@ -7,16 +7,19 @@ interface CardProps {
 }
 
 const CardComponent: React.FC<CardProps> = ({ title, imageUrl, href }) => {
-  return (
-    <Link href={href}>
-      <div className="aspect-w-5 aspect-h-3 overflow-hidden rounded shadow-lg bg-white hover:bg-gray-100 transition-colors duration-300 cursor-pointer">
-        <img className="object-cover" src={imageUrl} alt="Card" />
-        <div className="p-4">
-          <div className="font-bold text-xl mb-2">{title}</div>
+    return (
+      <Link href={href}>
+        <div className="w-[14em] h-[10em] overflow-hidden rounded shadow-lg bg-white hover:bg-gray-100 transition-colors duration-300 cursor-pointer">
+          <div className="h-[8em] overflow-hidden"> {/* Set the height to half of the card */}
+              <img className="w-full h-full object-cover" src={imageUrl} alt="Card" />
+          </div>
+          <div className="p-4">
+            <div className="font-bold text-xl mb-2">{title}</div>
+          </div>
         </div>
-      </div>
-    </Link>
-  );
-};
-
-export default CardComponent;
+      </Link>
+    );
+  };
+  
+  export default CardComponent;
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,20 @@ import CardComponent from './components/Cards/CardComponent';
 
 // Placeholder data for the cards
 const cardData = [
-  { title: "Card 1", imageUrl: "/path-to-image1.jpg", href: "/page1" },
-  { title: "Card 2", imageUrl: "/path-to-image2.jpg", href: "/page2" },
-  { title: "Card 3", imageUrl: "/path-to-image3.jpg", href: "/page3" },
-  { title: "Card 4", imageUrl: "/path-to-image4.jpg", href: "/page4" },
-  { title: "Card 5", imageUrl: "/path-to-image5.jpg", href: "/page5" },
-  { title: "Card 6", imageUrl: "/path-to-image6.jpg", href: "/page6" },
+  { title: "Card 1", imageUrl: "/images/bluenight.gif", href: "/page1" },
+  { title: "Card 2", imageUrl: "/images/collosus.gif", href: "/page2" },
+  { title: "Card 3", imageUrl: "/images/greendeath.gif", href: "/page3" },
+  { title: "Card 4", imageUrl: "/images/orangeresting.gif", href: "/page4" },
+  { title: "Card 5", imageUrl: "/images/redwasteland.gif", href: "/page5" },
+  { title: "Card 6", imageUrl: "/images/samuraigraveyard.gif", href: "/page6" },
 ];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" sm:mt-32 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static h-auto lg:w-auto lg:bg-none">
+      <div className=" sm:mt-32 flex  items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black   lg:bg-none">
         
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
           {/* Map over the card data to render each card */}
           {cardData.map((card, index) => (
             <CardComponent
