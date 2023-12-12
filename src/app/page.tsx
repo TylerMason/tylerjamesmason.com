4fbdae9cb9c6f5d22973b679ed09fbccb28f26a1
TylerMason <tymasn@gmail.com>
2023-12-11 16:24:34 -0800

    still working on backgroud circle gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,32 @@
 import Image from 'next/image';
 import CardComponent from './components/Cards/CardComponent';
-
-// Placeholder data for the cards
-const cardData = [
-  { title: "Card 1", imageUrl: "/images/bluenight.gif", href: "/project1" },
-  { title: "Card 2", imageUrl: "/images/collosus.gif", href: "/project2" },
-  { title: "Card 3", imageUrl: "/images/greendeath.gif", href: "/project3" },
-  { title: "Card 4", imageUrl: "/images/orangeresting.gif", href: "/project4" },
-  { title: "Card 5", imageUrl: "/images/redwasteland.gif", href: "/project5" },
-  { title: "Card 6", imageUrl: "/images/samuraigraveyard.gif", href: "/project6" },
-];
+import Header from './components/Header/Header';
+import cardData from '../../data/cardData';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" sm:mt-32 flex  items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black   lg:bg-none">
-        
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
-          {/* Map over the card data to render each card */}
-          {cardData.map((card, index) => (
-            <CardComponent
-              key={index}
-              title={card.title}
-              imageUrl={card.imageUrl}
-              href={card.href}
-            />
-          ))}
+    <>
+      <Header title="Home"/>
+      <main className="flex min-h-screen flex-col items-center bg-gray-700 justify-between p-24 relative">
+        {/* Gradient circle with adjusted size, position, and z-index */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] rounded-full bg-gradient-to-r from-blue-500 to-blue-300 opacity-75 z-0"></div>
+        <div className="sm:mt-32 flex items-end justify-center bg-gradient-to-t from-white via-white dark:from-blue-500 dark:via-blue-500 lg:bg-none z-10">
+          
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+            {cardData.map((card, index) => (
+              <CardComponent
+                key={index}
+                title={card.title}
+                imageUrl={card.imageUrl}
+                href={card.href}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      
-    </main>
+      </main>
+    </>
   );
 }
+
+
+
