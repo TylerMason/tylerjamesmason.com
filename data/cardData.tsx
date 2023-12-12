da3b8be065b6bd38da1a8e53ac93be6cb743b061
TylerMason <tymasn@gmail.com>
2023-12-11 16:25:23 -0800

    added some props and moved temporary static data

---
+++ data/cardData.tsx
@@ -0,0 +1,12 @@
+
+// Placeholder data for the cards
+const cardData = [
+    { title: "Card 1", imageUrl: "/images/bluenight.gif", href: "/project1" },
+    { title: "Card 2", imageUrl: "/images/collosus.gif", href: "/project2" },
+    { title: "Card 3", imageUrl: "/images/greendeath.gif", href: "/project3" },
+    { title: "Card 4", imageUrl: "/images/lushwaterfalls.gif", href: "/project4" },
+    { title: "Card 5", imageUrl: "/images/redwasteland.gif", href: "/project5" },
+    { title: "Card 6", imageUrl: "/images/samuraigraveyard.gif", href: "/project6" },
+  ];
+  
+  export default cardData
